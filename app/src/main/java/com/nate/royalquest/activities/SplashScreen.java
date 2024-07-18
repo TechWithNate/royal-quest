377e78abcd5421878f8901c739c3d96c045a9585
NathanDzreke-Poku <dzrekenathan2002@gmail.com>
2024-07-18 18:19:32 +0000

    Initial Commit

--- app/src/main/java/com/nate/royalquest/activities/SplashScreen.java
+++ app/src/main/java/com/nate/royalquest/activities/SplashScreen.java
@@ -11,7 +11,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.nate.royalquest.MainActivity;
 import com.nate.royalquest.R;
 
 @SuppressLint("CustomSplashScreen")
